3fa4a61905815f2962a8e3bac5b51eb38f89b8ce
yeol <yeol8810@gmail.com>
2023-10-21 19:37:39 +0900

    study 정리

--- calculator/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "singleQuote": false,
-    "trailingComma": "es5",
-    "semi": true,
-    "useTabs": false,
-    "tabWidth": 2,
-    "printWidth": 80,
-    "arrowParens": "always"
-}
